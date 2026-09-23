ad6e76498b4536da10e33abc0f49162990ab0b86
Wispy <elizavoets@gmail.com>
2026-09-23 14:03:03 +0200

    Base rotation

--- ProjectIgnite_team5/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ ProjectIgnite_team5/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -8,24 +8,34 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector3 MoveDirection;
 
-    // Variables
+    // Private variables
     private float _movementSpeed = 5f;
     private CharacterController _controller;
     private Vector2 _moveInput;
-   
+
+    private float _lookSpeed = 2f;
+    private float _lookLimit = 50f;
+
+    private Vector2 _mouseRot;
+    private float _mouseY;
 
     private float _jumpForce = 10f;
+
+    [SerializeField]
+    private Transform _playerBody;
 
 
     void Awake()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         _controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         _controller.Move(MoveDirection * _movementSpeed * Time.deltaTime);
-
+        _playerBody.Rotate(Vector2.up * _mouseRot.x * _lookSpeed);
     }
 
     //This will be called in the player's properties. Look at: player input > events > player > move.
@@ -38,5 +48,10 @@ public class PlayerMovement : MonoBehaviour
     public void Jump(InputAction.CallbackContext Context) 
     {
         //_Rigidbody.AddForce(Vector3.up * _jumpForce * _Rigidbody.mass, ForceMode.Impulse);
+    }
+
+    public void Look(InputAction.CallbackContext Context) 
+    { 
+        _mouseRot = Context.ReadValue<Vector2>();
     }
 }
--- ProjectIgnite_team5/Assets/Scripts/PlayerScripts/PlayerRotation.cs
+++ ProjectIgnite_team5/Assets/Scripts/PlayerScripts/PlayerRotation.cs
@@ -2,17 +2,24 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
-    private PlayerMovement _playerMovement;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        _playerMovement = GetComponent<PlayerMovement>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+    }
+
+    void RotatePlayerX(float xRot) 
+    { 
         
     }
 }
